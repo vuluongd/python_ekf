74fea9047bd9defb3e462e7f6547f460d6cf107e
vuluongd <vul45845@gmail.com>
2025-12-30 16:30:35 +0700

    d

--- ekf.py
+++ ekf.py
@@ -1,4 +1,23 @@
 import numpy as np 
 
 class EKF:
-    def __init__(self, Q, R)
+    def __init__(self, Q, R):
+        self.X = np.zeros(4)
+        self.P = np.eye(4)
+        self.Q = Q
+        self.R = R
+
+    def predict(self, acc, dt):
+        F = np.array([1,0,dt,0],
+                     [0,1,0,dt],
+                     [0,0,1,0],
+                     [0,0,0,1])
+        B = np.array([0.5*dt**2,0],
+                     [0,0.5*dt**2],
+                     [dt,0],
+                     [0,dt])
+        self.X = F @ self.X + B @ acc
+        self.P = F @ self.P @ F.T + self.Q
+
+    def update()
+        
