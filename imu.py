3b7126fbd905423b36bd571ab1f1375f0e7b700b
vuluongd <vul45845@gmail.com>
2025-12-31 02:32:56 +0700

    d

--- imu.py
+++ imu.py
@@ -1,17 +1,45 @@
 import numpy as np
 
 class imu:
-    def __init__(self, bias):
-        self.bias = bias
-        self.vel = np.zeros(2)
-        self.pos = np.zeros(2)
+    def _init_(self, dt, acc_bias = 0.001, gyro_bias = 0.0001):
+        self.dt = dt
+        self.g = 9.81
+
+        self.Ba = acc_bias
+        self.Bg = gyro_bias
+
+        self.v = np.zeros(2)
+        self.p = np.zeros(2)
+
+        self.dv = np.zeros(2)
+        self.dp = np.zeros(2)
+
+    def update(self, acc, dtheta):
+        dt = self.dt
+
+        a_B = acc + self.Ba 
+
+        self.dv = (
+            self.dv 
+            + self.Ba * dt
+            + self.g * dtheta*dt
+            - self.g* self.Bg *dt**2
+        )
+
+        self.dp = (
+            self.dp 
+            + self.dv * dt
+            + 0.5 * self.Ba * dt**2
+            - 0.5 * self.g * dtheta * dt**2
+            - (1/6) * self.g * self.Bg * dt**3
+        )
+
+        self.v = (self.v - self.dv) + a_B * dt + self.dv
+
+        self.p = (self.p - self.dp) + (self.v - self.dv)*dt + 0.5*a_B*dt**2
+
+        return self.v, self.p
 
 
-    def update(self, acc, dt):
-        acc_m = acc + self.bias * np.random.randn(2)
-        self.vel += acc_m * dt
-        self.pos += self.vel * dt + 0.5 * acc_m *dt**2
-        return acc_m
-    
         
         
